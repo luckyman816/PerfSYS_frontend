2c3616b32bc9ba7ec004b3678a91eb0f43fd8a67
Michael Moore <zhangyunpei816@gmail.com>
2023-12-08 19:49:57 -0800

    API update

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 import store from 'store/index';
 import { LOGOUT } from '../actions/types';
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || `http://${window.location.hostname}:5000/api`,
+  // baseURL: process.env.REACT_APP_API_URL || `http://${window.location.hostname}:5000/api`,
+  baseURL: process.env.REACT_APP_API_URL || 'mongodb+srv://luckyangel314:OAql28IVemHJQUYU@cluster0.hrff1nk.mongodb.net/api',
   headers: {
     'Content-Type': 'application/json'
   }
