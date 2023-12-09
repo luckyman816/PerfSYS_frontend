08c6c6e5a53a2c943497f1b308d3ae148656c70e
Michael Moore <zhangyunpei816@gmail.com>
2023-12-09 06:05:09 -0800

    modify api (host vercel)

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,7 @@ import store from 'store/index';
 import { LOGOUT } from '../actions/types';
 const api = axios.create({
   // baseURL: process.env.REACT_APP_API_URL || `http://${window.location.hostname}:5000/api`,
-  baseURL: process.env.REACT_APP_API_URL || 'mongodb+srv://luckyangel314:OAql28IVemHJQUYU@cluster0.hrff1nk.mongodb.net/api',
+  baseURL: process.env.REACT_APP_API_URL || 'https://perfsys-backend.vercel.app/api',
   headers: {
     'Content-Type': 'application/json'
   }
